85e54a4d757d78a53c1aa32b793885fa358cb7b7
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-22 22:09:40 -0400

    Test: Vercel erro

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+# Importação das bibliotecas
 from flask import Flask, render_template, request
 from random import randint
 
+
+# Variável principal
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+
+# Definição de rotas
+@app.route('/', methods=['GET', 'POST'])
 def home():
     valor = str(request.form.get('valor'))
-    valor_quest = str(randint(1,5))
-    print(type(valor))
-    if request.method =="GET":
+    valor_quest = str(randint(1, 5))
+    if request.method == "GET":
         return render_template('home.html')
     else:
         if valor == valor_quest:
             return '<h1>Você ganhou</h1>'
         else:
-            return '<h1>você perdeu</h1>' 
+            return '<h1>você perdeu</h1>'
 
 
-
+# Rota inválida
 @app.route('/<string:nome>')
 def error(nome):
     return '<h1>Essa página não existe</h1>'
 
+
+# Run server
 if __name__ == '__main__':
     app.run(debug=True)
-
